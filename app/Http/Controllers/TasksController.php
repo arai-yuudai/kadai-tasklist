197f54ad29c5d6c48c060217122533acaabbb1ce
araiyuudai <yu.ara@kdk-nagano.co.jp>
2021-11-04 09:15:09 +0000

    kadai

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -8,9 +8,9 @@ use App\Task;
 
 class TasksController extends Controller
 {
-    // public function __construct(){
-    // $this->middleware('auth', ['only' => ['show']]);
-    // }
+    public function __construct(){
+    $this->middleware('auth', ['only' => ['show','index','create']]);
+    }
     
     public function index()
     {
