eb678865dce85a33f72e7066f47a8f959c453b52
araiyuudai <yu.ara@kdk-nagano.co.jp>
2021-11-04 09:27:49 +0000

    kadai

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
-
    
 
 // ユーザ登録
@@ -17,5 +16,8 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-     Route::resource('tasks', 'TasksController');
+     // Route::get('tasks', 'TasksController@index');
+     // Route::resource('tasks', 'TasksController'); 
+      Route::resource('tasks', 'TasksController', ['only' => ['index', 'show']]);
+      Route::resource('tasks', 'TasksController', ['only' => ['show']]);
 });
